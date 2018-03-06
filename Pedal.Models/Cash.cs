2eb1a2edf7bb0533baae1d205a8d138067de35aa
Baki Forhad <bakiforhad11@gmail.com>
2018-03-07 01:59:42 +0600

    booking,rent,cyclestatus model added

---
+++ Pedal.Models/Cash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedal.Models
+{
+    public class Cash
+    {
+        [Required]
+        public int CashId { get; set; }
+        [Required]
+        public int RentId { get; set; }
+        [Required]
+        public int RentedHour { get; set; }
+        [Required]
+        public int StoreId { get; set; }
+        [Required]
+        public int AdminId { get; set; }
+        [Required]
+        public DateTime CashReceiveTime { get; set; }
+    }
+}
---
+++ Pedal.Models/Rent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedal.Models
+{
+    public class Rent
+    {
+        public int RentId { get; set; }
+        public int CycleId { get; set; }
+        public int UserId { get; set; }
+        public int TotalRentedHour { get; set; }
+        public int RentedFromStoreId { get; set; }
+        public int ToBeSubmittedStoreId { get; set; }
+        public DateTime RentedTime { get; set; }
+        public DateTime? ReturnTime { get; set; }
+        public int? BookingId { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
