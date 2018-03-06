0f75521fe9d39a905fcb898ce4246c0220ab226c
Baki Forhad <bakiforhad11@gmail.com>
2018-03-07 02:49:56 +0600

    repository and interfaces added

---
+++ Pedal.Data/PedalDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Pedal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedal.Data
+{
+    public class PedalDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Cash> CashBook { get; set; }
+    }
+}
