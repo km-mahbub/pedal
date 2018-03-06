765959cd3bc82b1966db2bd5fd164314b10942db
Ehsanul_Haque <ehsan.ul_haque@yahoo.com>
2018-03-07 00:49:34 +0600

    Cycle Added

---
+++ Pedal.Models/Cycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedal.Models
+{
+    public class Cycle
+    {
+
+         public int Cycleid { get; set; }
+         public int Cycletypeid { get; set; }
+         public int Companyid { get; set; }
+         public string Status { get; set; }
+         public int Rentedhour { get; set; }
+         public int Currentstoreid { get; set; }
+         public int? Storeid { get; set; }
+
+        
+    }
+
+}
+
