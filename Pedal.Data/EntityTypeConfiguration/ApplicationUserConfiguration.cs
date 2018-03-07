85fb72f46c92c8d974c2013925b4387c28b6db46
K.M. Mahbubuzzaman <iamdipto7@google.com>
2018-03-08 02:42:13 +0600

    Applicationuser configuration added

---
+++ Pedal.Data/EntityTypeConfiguration/ApplicationUserConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Pedal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedal.Data.EntityTypeConfiguration
+{
+    class ApplicationUserConfiguration: EntityTypeConfiguration<ApplicationUser>
+    {
+        public ApplicationUserConfiguration()
+        {
+            Property(a => a.FirstName)
+                .IsOptional()
+                .HasMaxLength(64);
+            Property(a => a.LastName)
+                .IsOptional()
+                .HasMaxLength(64);
+            Property(a => a.DateOfBirth)
+                .HasColumnAnnotation("Default", "4-4-1900");
+            Property(a => a.Gender)
+                .IsOptional();
+            Property(a => a.NationalId)
+                .IsOptional();
+            Property(a => a.PassportNumber)
+                .IsOptional();
+            Property(a => a.DrivingLicense)
+                .IsOptional();
+
+            HasOptional(c => c.Address)
+                .WithMany(a => a.ApplicationUsers)
+                .HasForeignKey(c => c.AddressId);
+        }
+    }
+}
