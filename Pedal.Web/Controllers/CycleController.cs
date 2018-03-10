79df7bc1f568b909618e365b081cb77f26bda851
K.M. Mahbubuzzaman <iamdipto7@google.com>
2018-03-10 19:02:24 +0600

    cycle controller

---
+++ Pedal.Web/Controllers/CycleController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pedal.Models;
+using Pedal.Repositories;
+using Pedal.Repositories.Interfaces;
+using Pedal.Web.Models.ViewModels;
+
+namespace Pedal.Web.Controllers
+{
+    public class CycleController : Controller
+    {
+        IUnitOfWork _unitOfWork = new UnitOfWork();
+        // GET: Cycle
+        public ActionResult Index()
+        {
+            IEnumerable<CycleViewModel> cList = _unitOfWork.CycleRepository.GetAll();
+            return View(cList);
+        }
+
+        // GET: Cycle/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cycle/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cycle/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cycle/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cycle/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cycle/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cycle/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Pedal.Web/Models/ViewModels/CycleViewModel.cs
+++ Pedal.Web/Models/ViewModels/CycleViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Pedal.Models;
 
 namespace Pedal.Web.Models.ViewModels
 {
     public class CycleViewModel
     {
+        public int? CycleId { get; set; }
+        public int? RentedHour { get; set; }
+        [Display(Name = "Select Type")]
+        public CycleType CycleType { get; set; }
+        [Display(Name = "Select Company")]
+        public int CompanyId { get; set; }
+        public IEnumerable<Company> Companies { get; set; }
+        [Display(Name = "Select Store")]
+        public int StoreId { get; set; }
+        public IEnumerable<Store> Stores { get; set; }
+        [Display(Name = "Select Status")]
+        public CycleStatusType CycleStatusType { get; set; }
     }
 }
